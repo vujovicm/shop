535e205b6ac982a98f651415001b5df067d93753
vujovicm <63158265+vujovicm@users.noreply.github.com>
2023-03-29 12:29:55 +0200

    formatting

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,35 +1,45 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function Dropdown({placeHolder, options, updateSortOption}) {
-    const[showDropdown, setShowDropdown]= useState(false);
-    const[selectedOption, setSelectedOption]= useState(null);
+export default function Dropdown({ placeHolder, options, updateSortOption }) {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
 
-    const sortCriteria = selectedOption === null ? placeHolder: selectedOption.label;
+  const sortCriteria =
+    selectedOption === null ? placeHolder : selectedOption.label;
 
-    const toggleDropdown = () => {
-        setShowDropdown(!showDropdown);
-        }
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
 
-    const selectOption = (option) => {
-        //console.log(option);
-        setSelectedOption(option);
-        setShowDropdown(false);
-        updateSortOption(option); // call parent function to send this option to it
-    }
+  const selectOption = (option) => {
+    //console.log(option);
+    setSelectedOption(option);
+    setShowDropdown(false);
+    updateSortOption(option); // call parent function to send this option to it
+  };
 
-return (
+  return (
     <div className="dropdown-container">
-        <div className="dropdown-input" onClick={toggleDropdown}> {sortCriteria} </div>
-        {showDropdown && 
-            <div className="dropdown-menu">
-                {options.map(option => {
-                    return <div key={option.id} onClick={() => selectOption(option)} className="dropdown-item" value={option.id}>
-                        {option.label}
-                    </div>
-                })}
-            </div> 
-        }
-
+      <div className="dropdown-input" onClick={toggleDropdown}>
+        {" "}
+        {sortCriteria}{" "}
+      </div>
+      {showDropdown && (
+        <div className="dropdown-menu">
+          {options.map((option) => {
+            return (
+              <div
+                key={option.id}
+                onClick={() => selectOption(option)}
+                className="dropdown-item"
+                value={option.id}
+              >
+                {option.label}
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
-)
+  );
 }
